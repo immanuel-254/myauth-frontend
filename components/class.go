1d82f411229ee75daa577282764131a8c4bef147
immanuel-254 <njugunaimmanuel1@outlook.com>
2025-03-02 22:01:22 +0300

    The stats are working and the home page is coming along

--- components/class.go
+++ components/class.go
@@ -20,6 +20,11 @@ const (
 	DropDownMenuContent = "z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2"
 	DropDownLabel       = "px-2 py-1.5 text-sm font-semibold"
 	DropDownMenuItem    = "relative flex cursor-default select-none items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0"
+
+	Pagination        = "mx-auto flex w-full justify-center"
+	PaginationContent = "flex flex-row items-center gap-1"
+
+	Select = "flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background data-[placeholder]:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1"
 )
 
 func ButtonVariant(variant string) string {
---
+++ index.js
@@ -0,0 +1,8 @@
+import { createIcons, ChevronLeft, ChevronRight} from 'lucide';
+
+createIcons({
+  icons: {
+    ChevronLeft,
+    ChevronRight
+  }
+});
